44c9634b4489a5b3433cdbbaf32dedb994bda93b
Luke Bacon <luke@equivalentideas.com>
2018-02-07 20:10:12 +1100

    Handle redirects to non-data page and skip
    This page is redirecting to a maintenance page, scraping stops...
    https://web.archive.org/web/20180105131324/https://tenders.nsw.gov.au/

--- scraper.rb
+++ scraper.rb
@@ -12,6 +12,32 @@ def web_archive(url)
   archive_request_response.headers[:link].split(", ")[2][/\<.*\>/].gsub(/<|>/, "")
 end
 
+def skip_bad_archive(url)
+  puts "Unable to parse the response from #{url}, skipping"
+end
+
+def record_release(release, archive_timestamp)
+  release["awards"].each do |award|
+    url = "https://tenders.nsw.gov.au/?event=public.api.contract.view&CNUUID=#{award["CNUUID"]}"
+
+    puts "Getting contract data from #{url}"
+
+    record = {
+      scraped_at: Date.today.to_s,
+      web_archive_url: web_archive(url),
+      CNUUID: award["CNUUID"],
+      ocid: release["ocid"],
+      data_blob: Net::HTTP.get(URI(url)),
+      archive_timestamp: archive_timestamp
+    }
+
+    puts "Saving contract data from #{url}"
+    ScraperWiki.save_sqlite([:CNUUID, :archive_timestamp], record)
+
+    sleep 3
+  end
+end
+
 puts "Getting list of archived searches from https://web.archive.org/web/timemap/json/http://tenders.nsw.gov.au/?event=public.api.contract.searc"
 timemap = JSON.parse(
   Net::HTTP.get(
@@ -23,30 +49,17 @@ archive_times = timemap[1..-1].map { |t| t[1] }
 archive_times.each do |archive_timestamp|
   if (ScraperWiki.select("archive_timestamp from data where archive_timestamp='#{archive_timestamp}'").empty? rescue true)
     puts "Getting archived search data from #{archived_search_url(archive_timestamp)}"
-    archived_search_data = JSON.parse(
-      Net::HTTP.get(URI(archived_search_url(archive_timestamp)))
-    )
 
-    archived_search_data["releases"].each do |release|
-      release["awards"].each do |award|
-        url = "https://tenders.nsw.gov.au/?event=public.api.contract.view&CNUUID=#{award["CNUUID"]}"
+    begin
+      archived_search_data = JSON.parse(
+        RestClient.get(archived_search_url(archive_timestamp))
+      )
 
-        puts "Getting contract data from #{url}"
-
-        record = {
-          scraped_at: Date.today.to_s,
-          web_archive_url: web_archive(url),
-          CNUUID: award["CNUUID"],
-          ocid: release["ocid"],
-          data_blob: Net::HTTP.get(URI(url)),
-          archive_timestamp: archive_timestamp
-        }
-
-        puts "Saving contract data from #{url}"
-        ScraperWiki.save_sqlite([:CNUUID, :archive_timestamp], record)
-
-        sleep 3
+      archived_search_data["releases"].each do |release|
+        record_release(release, archive_timestamp)
       end
+    rescue JSON::ParserError
+      skip_bad_archive(archived_search_url(archive_timestamp))
     end
   end
 end
